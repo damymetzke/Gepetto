8b4f5d97ef1b3ed82549ea06256a6324e89f1bcf
DamyMetzke <damy.metzke@gmail.com>
2020-03-10 11:59:36 +0100

    added body styling.

--- html/scripts/main.js
+++ html/scripts/main.js
@@ -4,15 +4,19 @@ function createWindow()
 {
     // Create the browser window.
     let win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1000,
+        height: 800,
+        minWidth: 700,
+        minHeight: 500,
         webPreferences: {
             nodeIntegration: true
-        }
+        },
+        useContentSize: true
     });
 
     // and load the index.html of the app.
     win.loadFile('index.html');
+    win.maximize();
 }
 
 app.whenReady().then(createWindow);
