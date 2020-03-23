9bb77fad7b516c93ecb8a6e4f9fc29aa6a0a9a06
DamyMetzke <damy.metzke@gmail.com>
2020-03-23 12:34:09 +0100

    [create SVG import tab] written basic styling.

--- html/scripts/index-main.js
+++ html/scripts/index-main.js
@@ -11,4 +11,18 @@ export function Run(root)
 
     const target = document.getElementById("main").children[0];
     ContentLoader.LoadContent(new ContentLoader.Content(ObjectEditorMain.Run, "./object-editor.html"), target);
+
+    // const Dialog = require("electron").remote.dialog;
+    // console.log(Dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] }));
+    const BrowserWindow = require("electron").remote.BrowserWindow;
+    let temp = new BrowserWindow({
+        width: 300,
+        height: 200,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        useContentSize: true
+    });
+
+    temp.loadFile("./svg-import.html");
 }
