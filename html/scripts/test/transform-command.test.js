ef3e1377615dbe2a8c0189c53c86372e48e5bb3a
DamyMetzke <damy.metzke@gmail.com>
2020-03-28 17:37:39 +0100

    [implement transform commands] added more testing for translate and scale commands.

--- html/scripts/test/transform-command.test.js
+++ html/scripts/test/transform-command.test.js
@@ -10,10 +10,19 @@ let shearYCommand = new TransformCommand("SHEARY", 999, 60);
 
 test("Translate Command", function ()
 {
-    expect(translateCommand.CreateMatrix()).toStrictEqual(new Transform([1, 0, 0, 1, 10, 20]));
+    const transform = translateCommand.CreateMatrix();
+    expect(transform).toStrictEqual(new Transform([1, 0, 0, 1, 10, 20]));
+
+    expect(transform.MultiplyVector([0, 0])).toEqual([10, 20]);
+    expect(transform.MultiplyVector([-20, 500])).toEqual([-10, 520]);
+
 });
 
 test("Scale Command", function ()
 {
-    expect(scaleCommand.CreateMatrix()).toStrictEqual(new Transform([30, 0, 0, 40, 0, 0]));
+    const transform = scaleCommand.CreateMatrix();
+    expect(transform).toStrictEqual(new Transform([30, 0, 0, 40, 0, 0]));
+
+    expect(transform.MultiplyVector([0, 0])).toStrictEqual([0, 0]);
+    expect(transform.MultiplyVector([-100, 23])).toStrictEqual([-3000, 920]);
 });
