b3a2b0565c59862e0338d5c776bfec01ab4fed76
DamyMetzke <damy.metzke@gmail.com>
2020-03-30 14:41:08 +0200

    [] documented transform.

--- html/scripts/back/main.js
+++ html/scripts/back/main.js
@@ -5,7 +5,6 @@ let window = null;
 
 function createWindow()
 {
-    // Create the browser window.
     window = new BrowserWindow({
         width: 1000,
         height: 800,
@@ -17,9 +16,7 @@ function createWindow()
         useContentSize: true
     });
 
-    // and load the index.html of the app.
     window.loadFile('index.html');
-    //win.loadFile('object-editor.html');
     window.maximize();
 
     Init(window);
--- html/scripts/core/transform.js
+++ html/scripts/core/transform.js
@@ -11,7 +11,18 @@ class Transform
 {
     classIdentifier = "Transform";
     matrix = [1, 1, 1, 1, 1, 1];
-
+    /**
+     * linear interpolate between this and target.
+     * 
+     * each value in the matrix is interpolated using the following formula:
+     *     this + (progress * (target - this))
+     * 
+     * @see https://en.wikipedia.org/wiki/Linear_interpolation
+     * 
+     * @param {Transform} target a transform which will be the end point of the lerp
+     * @param {Number} progress a unit value (0.0 - 1.0) which represents the progress of the lerp;
+     * 0 represents this transform, 1 represents the target transform.
+     */
     Lerp(target, progress)
     {
         if (target.classIdentifier !== "Transform")
@@ -28,8 +39,26 @@ class Transform
             this.matrix[5] + (progress * (target.matrix[5] - this.matrix[5]))
         ]);
     }
+    /**
+     * the same as lerp, however rather than directly lerping to the end point the rotation is lerped seperately.
+     * 
+     * the calculation is nearly the same as lerp, however before interpolating the rotation is extracted and interpolated seperately.
+     * 
+     * @see Transform.Lerp
+     * 
+     * @param {Transform} target a transform which will be the end point of the slerp
+     * @param {Number} progress a unit value (0.0 - 1.0) which represents the progress of the slerp;
+     * 0 represents this transform, 1 represents the target transform.
+     */
     Slerp(target, progress) { }
 
+    /**
+     * a multiplication, or dot product, operation with this on the left and right on the right.
+     * 
+     * @see https://en.wikipedia.org/wiki/Matrix_multiplication
+     * 
+     * @param {Transform} right right hand side of the operation
+     */
     MultiplyMatrix(right)
     {
         return new Transform([
@@ -41,6 +70,18 @@ class Transform
             this.matrix[1] * right.matrix[4] + this.matrix[3] * right.matrix[5] + this.matrix[5],
         ]);
     }
+    /**
+     * a multiplication, or dot product, operation with this on the left and right on the right.
+     * 
+     * the right hand side looks like this:
+     *     ┌.x.┐
+     *     |.y.|
+     *     └.1.┘
+     * 
+     * @see https://en.wikipedia.org/wiki/Matrix_multiplication
+     * 
+     * @param {Number[2]} right right hand side of the operation.
+     */
     MultiplyVector(right)
     {
         return [
