465306b7a3dfb9c39a2637aebe0205c524f7c67e
DamyMetzke <damy.metzke@gmail.com>
2020-03-31 14:09:04 +0200

    [] documented DrawObjectXmlhandler

--- html/scripts/back/draw-object-xmlhandler.js
+++ html/scripts/back/draw-object-xmlhandler.js
@@ -1,5 +1,33 @@
 const xml = require("xml2js");
 
+/**
+ * convert a standart svg file into an object xml file.
+ *
+ * in order to convert a file the following operations are done:
+ *
+ * - load the svg file but remove the root tag.
+ * - if a viewport has been set use it to convert all values to a space between -100 and 100 (not implemented yet).
+ * - return the result.
+ * 
+ * @param {String} svgContent svg-formatted string which will be converted
+ * 
+ * 
+ * @return {{}}
+ * get the converted xml string, or error code on failure.
+ * returns one of the following objects:
+ * ```js
+ * //on success
+ * {
+ *     success: true,
+ *     content: String() //xml string
+ * }
+ * //on failure
+ * {
+ *     success: false,
+ *     error: String() //error string
+ * }
+ * ```
+ */
 function SvgToObjectXml(svgContent)
 {
     console.log(svgContent);
@@ -50,5 +78,17 @@ function SvgToObjectXml(svgContent)
 
     return result;
 }
-
+/**
+ * @alias module:DrawObjectXmlhandler.svgToObjectXml
+ */
 module.exports.SvgToObjectXml = SvgToObjectXml;
+
+/**
+ * this module is in charge of all DrawObject xml functionality.
+ *
+ * DrawObject xml is almost the same as svg, however the root tag is omitted and the screen space is always from -100 to 100.
+ *
+ * @module DrawObjectXmlhandler
+ *
+ * @see DrawObject
+ */
