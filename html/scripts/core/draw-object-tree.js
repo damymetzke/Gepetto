8ff6fd70e8578cdaefed982b793a32c4930bb165
DamyMetzke <damy.metzke@gmail.com>
2020-04-07 10:02:10 +0200

    [] added pure object functions to DrawObjectTree.

--- html/scripts/core/draw-object-tree.js
+++ html/scripts/core/draw-object-tree.js
@@ -29,6 +29,41 @@ class DrawObjectTree
         return (name in this.objects);
     }
 
+    ToPureObject()
+    {
+        let result = {
+            rootObjects: [],
+            objects: {}
+        };
+
+        for (name in this.objects)
+        {
+            result.objects[name] = this.objects[name].ToPureObject();
+        }
+
+        for (let i = 0; i < this.rootObjects.length; ++i)
+        {
+            result.rootObjects.push(result.objects[this.rootObjects[i].name]);
+        }
+
+        return result;
+    }
+
+    FromPureObject(object)
+    {
+        for (name in object.objects)
+        {
+            let newDrawObject = new DrawObject();
+            newDrawObject.FromPureObject(object.objects[name]);
+            this.objects[name] = newDrawObject;
+        }
+
+        for (let i = 0; i < object.rootObjects.length; ++i)
+        {
+            this.rootObjects.push(this.objects[object.rootObjects[i]]);
+        }
+    }
+
     constructor(rootObjects = [])
     {
         this.rootObjects = rootObjects;
