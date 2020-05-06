3cdd6574945cf86f8964252ce0d6409fd04329bb
Damy Metzke <damy.metzke@gmail.com>
2020-05-06 11:36:32 +0200

    added event listners (untested).

--- src/front/object-editor-alt/object-editor-alt-main.ts
+++ src/front/object-editor-alt/object-editor-alt-main.ts
@@ -3,20 +3,42 @@ import { SubDocHandler } from "../global/subdoc.js";
 
 const { TransformCommand } = require("electron").remote.require("../core/core");
 
-const eventListners = {
-    OnChangeName: (event: KeyboardEvent) =>
-    {
+const { ipcRenderer } = require("electron");
 
+interface IpcUpdateObject
+{
+    name: string;
+}
+
+interface IpcAddTransformCommand
+{
+    command: {};
+}
+
+
+const eventListners = {
+    OnChangeName: (event: KeyboardEvent, root: SubDocHandler) =>
+    {
+        if (event.key !== "Enter")
+        {
+            return;
+        }
+
+        ipcRenderer.invoke("update-object", <IpcUpdateObject>{
+            name: (<HTMLInputElement>root.GetElementBySid("property--name-input")).value
+        });
     },
     OnAddTransformCommand: (event: MouseEvent, template: any/*todo: change into transformcommand*/) =>
     {
-
+        ipcRenderer.invoke("add-transform-command-alt", <IpcAddTransformCommand>{
+            command: template.ToPureObject()
+        });
     }
 };
 
 function SetupEventListners(root: SubDocHandler)
 {
-    root.GetElementBySid("property--name-input").addEventListener("keypress", eventListners.OnChangeName);
+    root.GetElementBySid("property--name-input").addEventListener("keypress", (event: KeyboardEvent) => { eventListners.OnChangeName(event, root); });
     Array.from(root.GetElementBySid("property--transform-add-controls").children).forEach((child: HTMLElement | SVGElement) =>
     {
         const type = child.dataset.transformCommandType;
