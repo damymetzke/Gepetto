f1cdf4e45c789b66a1b85d71c8e3dcfe13a27351
Damy Metzke <damy.metzke@gmail.com>
2020-05-27 11:38:30 +0200

    clean-directories now uses config file.

--- node_scripts/clean.js
+++ node_scripts/clean.js
@@ -1,18 +1,26 @@
 const Color = require("./color.js");
 const del = require("del");
+const fs = require("fs");
 
-const CLEAN_DIRECTORIES = [
-    "./out/",
-    "./documentation/build/"
-];
+const CONFIG_PATH = "./config/build.config.json";
+const CONFIG_DATA = JSON.parse(fs.readFileSync(CONFIG_PATH));
+
+if (!("cleanDirectories" in CONFIG_DATA))
+{
+    Color.Error(
+        `key 'cleanDirectories' not found in config file (file){'${CONFIG_PATH}'}\n`
+        + `read config data:\n\t${
+        JSON.stringify(CONFIG_DATA, null, 2).replace(/\n/g, "\n\t")
+        }`);
+}
 
 Color.Log(`(head){Cleaning Directories:}
-${CLEAN_DIRECTORIES.reduce((accumelator, directory) => `${accumelator}* (file){"${directory}"}\n`, "")}`, [], {
+${CONFIG_DATA.cleanDirectories.reduce((accumelator, directory) => `${accumelator}* (file){"${directory}"}\n`, "")}`, [], {
     head: Color.HEADER,
     file: Color.FILE
 });
 
-CLEAN_DIRECTORIES.forEach(directory =>
+CONFIG_DATA.cleanDirectories.forEach(directory =>
 {
     del(directory).catch((error) =>
     {
