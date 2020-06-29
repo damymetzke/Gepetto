192b434b626d106300b91f6ece574ff165c2a94f
Damy Metzke <damy.metzke@gmail.com>
2020-06-29 12:22:35 +0200

    added SyncConnector_Back (untested).

---
+++ src/back/SyncConnector_Back.ts
@@ -0,0 +1,34 @@
+import { SyncConnector, SyncMessageAlt } from "./core/core";
+
+import { ipcMain, BrowserWindow } from "electron";
+
+export class SyncConnector_Back implements SyncConnector
+{
+    callback: (message: SyncMessageAlt) => void;
+    ipcChannel: string;
+    targetWindow: BrowserWindow;
+
+    send(message: SyncMessageAlt)
+    {
+        this.targetWindow.webContents.send(this.ipcChannel, message);
+    };
+    onRecieve(callback: (message: SyncMessageAlt) => void)
+    {
+        this.callback = callback;
+    }
+
+    constructor (ipcChannel: string, targetWindow: BrowserWindow)
+    {
+        this.ipcChannel = ipcChannel;
+        this.targetWindow = targetWindow;
+
+        ipcMain.handle(this.ipcChannel, (_event, message: SyncMessageAlt) =>
+        {
+            if (this.callback)
+            {
+                this.callback(message);
+            }
+        });
+    }
+
+}
