e0e3413be7aab9ffcbfc37e52f681539f177c02b
Damy Metzke <damy.metzke@gmail.com>
2020-07-30 23:51:42 +0200

    walk-directories now uses promises.

--- node_scripts/walk-directories.js
+++ node_scripts/walk-directories.js
@@ -1,89 +1,81 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 
-const Color = require("./color");
-
 const EMPTY_REGEX = /[^]*/;
+
+function addFile(relative, currentDirectory, file, root, test, fileCallback)
+{
+    return new Promise((resolve, reject) =>
+    {
+        const filePath = path.join(currentDirectory, file);
+        const nextRelative = path.join(relative, file);
+
+        fs.stat(filePath)
+            .then((stat) =>
+            {
+                if (stat && stat.isDirectory())
+                {
+                    resolve(Walk(root, nextRelative, test, fileCallback));
+                    return;
+                }
+
+                if (!test.test(file))
+                {
+                    resolve([]);
+                    return;
+                }
+
+                resolve([ nextRelative ]);
+
+                if (!fileCallback)
+                {
+                    return;
+                }
+
+                fs.readFile(filePath)
+                    .then((data) =>
+                    {
+                        fileCallback(data, relative, file);
+                    });
+            })
+            .catch((error) =>
+            {
+                reject(error);
+            });
+    });
+
+
+}
 
 function Walk(root, relative = "", test = EMPTY_REGEX, fileCallback = null)
 {
     return new Promise((resolve, reject) =>
     {
         const currentDirectory = path.join(root, relative);
-        fs.readdir(currentDirectory, (error, files) =>
-        {
-            if (error)
-            {
-                Color.Error(`Could not read directory (file){'${currentDirectory}'}\n${error}`);
-                return;
-            }
 
-            const result = files.reduce((accumelator, file) =>
+        fs.readdir(currentDirectory)
+            .then((files) =>
             {
-                const result = new Promise((resolve, reject) =>
+                let result = files.reduce((accumelator, file) =>
                 {
-                    const filePath = path.join(currentDirectory, file);
-                    fs.stat(filePath, (error, stat) =>
-                    {
-                        if (error)
-                        {
-                            Color.Error(`Could not read file stat (file){'${filePath}'}\n${error}`);
-                            reject({
-                                problem: `Could not read file stat '${filePath}'`,
-                                message: error
-                            });
-                            return;
-                        }
-
-                        const nextRelative = path.join(relative, file);
-                        if (stat && stat.isDirectory())
-                        {
-                            const result = Walk(root, nextRelative, test, fileCallback);
-                            result.then((fileList) =>
-                            {
-                                resolve(fileList);
-                            });
-                            return;
-                        }
-
-                        if (!test.test(file))
-                        {
-                            resolve([]);
-                            return;
-                        }
-
-                        resolve([nextRelative]);
-
-                        if (fileCallback)
-                        {
-                            fs.readFile(filePath, (error, data) =>
-                            {
-                                if (error)
-                                {
-                                    Color.Error(`Could not read file (file){'${currentDirectory}'} for file callback\n${error}`);
-                                    return;
-                                }
-
-                                fileCallback(data, relative, file);
-                            });
-                        }
-                    });
-                });
-                return [...accumelator, result];
-            }, []);
-
-            Promise.all(result).then((fileLists) =>
-            {
-                const fileList = fileLists.reduce((accumelator, value) =>
-                {
-                    return [...accumelator, ...value];
+                    return [
+                        ...accumelator,
+                        addFile(relative, currentDirectory, file, root, test, fileCallback)
+                    ];
                 }, []);
-                resolve(fileList);
-            }).catch((error) =>
+
+                return Promise.all(result);
+            })
+            .then((fileLists) =>
+            {
+                resolve(
+                    fileLists.reduce((accumelator, value) => [ ...accumelator, ...value ], [])
+                );
+            })
+            .catch((error) =>
             {
                 reject(error);
             });
-        });
     });
 }
 
