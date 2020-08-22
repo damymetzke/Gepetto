d5f1f83f0447462a533ef8e3a2f0b93acc5e7aef
Damy Metzke <damy.metzke@gmail.com>
2020-08-22 15:53:04 +0200

    implemented start script.

--- buildscript.config.js
+++ buildscript.config.js
@@ -1,10 +1,45 @@
-const { LOGGER } = require("node-build-util");
+const { LOGGER, runParallelScript, runNpm } = require("node-build-util");
+
+const path = require("path");
+
+const DEFAULT_PATHS = {
+    html: [
+        path.join(__dirname, "html"),
+        path.join(__dirname, "out")
+    ],
+    coreFront: [
+        "./src/core",
+        "./src/front/core"
+    ],
+    coreBack: [
+        "./src/core",
+        "./src/back/core"
+    ]
+};
 
 module.exports = {
     buildScripts: {
-        start: () =>
+        start: async () =>
         {
             LOGGER.log("Running script: 'start'");
+
+            const htmlAndSass = [
+                runParallelScript("std:fileSystem/copyFolder.js", ...DEFAULT_PATHS.html),
+                runNpm("old:compile-sass")
+            ];
+
+            await Promise.all([
+                runParallelScript("std:fileSystem/copyFolder.js", ...DEFAULT_PATHS.coreFront),
+                runParallelScript("std:fileSystem/copyFolder.js", ...DEFAULT_PATHS.coreBack),
+            ]);
+
+            await Promise.all([
+                runNpm("old:typescript-front"),
+                runNpm("old:typescript-back"),
+                ...htmlAndSass
+            ]);
+
+            runNpm("old:start");
         },
         test: () =>
         {
